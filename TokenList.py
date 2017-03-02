c6a28ddd9f4558becbb04a735eccc4dd8028e0f2
Julius <s0546803@htw-berlin.de>
2017-03-02 11:56:57 +0100

    TokenList angepasst

--- TokenList.py
+++ TokenList.py
@@ -1,34 +1,35 @@
 # -*- coding: utf-8 -*-
 #
 # Class contains all tokens + idf + tf_idf
+# when instanciated, all the values get calculated via the given document dictionary
 
 from math import log
 
-abs_tf = {}  # absolute term frequency
-norm_tf = {}  # augmented term frequency
-idf = {}  # inverse document frequency
-tf_idf = {}
-
-documents = {}
-
 
 class TokenList:
+
+    abs_tf = {}  # absolute term frequency
+    norm_tf = {}  # augmented term frequency
+    idf = {}  # inverse document frequency
+    tf_idf = {}
+
+    documents = {}
+
     def __init__(self, documents):
         self.documents = documents
         self.calc_term_frequencies_inverse_document_frequencies()
-        self.tf_idf = {}
-
-    def collect_abs_tf(self):
-        for document in documents:
-            for token in document.abs_tf.keys():
-                self.abs_tf[token].append(document.abs_tf[token])
 
     def collect_norm_tf(self):
-        for document in documents:
+        for document in self.documents:
             for token in document.norm_tf.keys():
                 self.norm_tf[token].append(document.norm_tf[token])
 
-    # Todo: Folgende beide Funktionen müssen noch an die neue Datenstruktur angepasst werden
+    def collect_abs_tf(self):
+        for document in self.documents:
+            for token in document.abs_tf.keys():
+                self.abs_tf[token].append(document.abs_tf[token])
+
+    # Todo: Folgend Funktion muss noch an die neue Datenstruktur angepasst werden
     def calc_inverse_document_frequencies(self):
         documents_including_token = 0
         for token in self.abs_tf.keys():
@@ -38,9 +39,14 @@ class TokenList:
             self.idf[token] = log(len(self.documents) / documents_including_token)
 
     def calc_term_frequencies_inverse_document_frequencies(self):
-        if (self.abs_tf is None) or (self.norm_tf is None):
-            self.calc_term_frequencies()
+        if self.norm_tf is None:
+            self.collect_norm_tf()
+        if self.abs_tf is None:
+            self.collect_abs_tf()
         if self.idf is None:
             self.calc_inverse_document_frequencies()
         for token in self.abs_tf.keys():
-            self.tf_idf = self.norm_tf[token] * self.idf[token]
+            token_total_norm_tf = 0
+            for entry in self.norm_tf[token]:
+                token_total_norm_tf += entry.pop()
+            self.tf_idf = token_total_norm_tf * self.idf[token]
