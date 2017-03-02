87ac6e9df89527710fe27fb2c6b918eb48f80d98
Laura Woelbeling <s0548106@htw-berlin.de>
2017-03-02 09:21:19 +0100

    Indexer als eigenständige Datei

--- indexer.py
+++ indexer.py
@@ -1,5 +1,7 @@
 # coding: utf8
-import math
+from langprocessor import LangProcessor
+import pickle
+from document import Document
 
 
 class Indexer:
@@ -59,3 +61,32 @@ class Indexer:
 
         return inv_posindex
 
+if __name__ == "__main__":
+    l = LangProcessor()
+    with open('helpers/docs.pickle', 'rb') as d:
+        docs = pickle.load(d)
+    csvfile = "out/word_lemma_mapping.csv"
+
+    print("Start Indexing...")
+    fobj_out = open(csvfile, "w")
+    fobj_out.close()
+
+    for docid in docs.keys():
+        doc = docs[docid]
+        print(docid)
+        doc.indexliste = l.get_index(doc.text, False, csvfile)
+
+    inv_index = Indexer.get_inverse_index(docs)
+    inv_posindex = Indexer.get_inverse_posindex(docs)
+
+    # Todo: Export des inv. Positionsindexes als JSON
+    # import json
+    # json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}])
+    # '["foo", {"bar": ["baz", null, 1.0, 2]}]'
+
+    # Todo: Hier erfolgt der Aufruf von TokenList und der Export der CSV-Datei
+
+    with open('helpers/invertierter_index.pickle', 'wb') as invf:
+        pickle.dump(inv_index, invf, protocol=2)
+    with open('helpers/invertierter_posindex.pickle', 'wb') as invpf:
+        pickle.dump(inv_posindex, invpf, protocol=2)
